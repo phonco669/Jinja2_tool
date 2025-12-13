3d50a193a8a8dea7c9f33478bad0616b6fa087ec
phonco669 <phonco669@126.com>
2025-12-13 16:04:09 +0800

    Optimize startup script interaction, add stop script and update docs

--- start.bat
+++ start.bat
@@ -12,6 +12,8 @@ where python >nul 2>&1 || ( echo Error: Python is not installed or not in PATH. 
 :: Param branches
 if /I "%MODE%"=="install" goto INSTALL
 if /I "%MODE%"=="noinstall" goto RUN
+if /I "%MODE%"=="run" goto RUN
+if /I "%MODE%"=="stop" goto STOP
 
 :: Auto install only when dependencies missing
 python -m pip show Flask >nul 2>&1 && python -m pip show Jinja2 >nul 2>&1 && goto RUN
@@ -22,6 +24,11 @@ python -m pip install -r requirements.txt -q --no-input -i https://pypi.tuna.tsi
 
 :RUN
 echo Starting Flask Server...
-python app.py
+start "Jinja2 Tool Server" cmd /c python app.py
+exit /b
 
 endlocal
+
+:STOP
+taskkill /FI "WINDOWTITLE eq Jinja2 Tool Server" /T /F >nul 2>&1
+exit /b
---
+++ stop.bat
@@ -0,0 +1,4 @@
+@echo off
+setlocal
+call "%~dp0start.bat" stop
+endlocal
